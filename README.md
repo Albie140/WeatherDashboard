2da583298e29d7843c3e48ad883f9ec06c105dd3
Alberte Laventure <alberte.laventure@gmail.com>
2020-09-13 23:51:34 -0400

    updated readme, index, and script files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WeatherDashboard
-For this assignemt, I created a 5 day forecast.
+For this assignemt, I created a 5 day forecast. This forecast will be beneficial for anyone who needs to know the weather conditions in their location. It will be particularly useful for travelers who want to find out the weather conditions at their future destination. A user can simply enter their desired city and click search weather. The result will be the 5 day forecast. 
--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-//Ajax call in seacrh box/onclick
-var apiKey="3c0180d657759505946b8abbfc2f67b7"
-$.ajax({
-url: "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=3c0180d657759505946b8abbfc2f67b7",
-method:"GET"
+$(document).ready(function(){
+
+    $("#add-city").on("click", function () {
+        console.log("Clicked");
+    var weatherInput =$("#weather-input").val(); 
+    var queryURL = "",
+    if (weatherInput !== ""){
+        $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/weather?id=" +weatherInput+ "990c20e7f9d2676532491d7e451773d2" + "&appid=" + "&units=imperial",
+                method: "GET",
+        })
+        console.log(data)
+
+    } else {
+        $("#error").html("Please choose a city");
+    }
+    })
+    .then(function(){
+        console.log ($this);
+    })
 })
-.then(function(weatherData) {
-    console.log(weatherData);
-})
+
+
+    //Ajax call in seacrh box/onclick
+
+// $.ajax({
+// url: "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=3c0180d657759505946b8abbfc2f67b7",
+// method:"GET"
+// })
+// .then(function(weatherData) {
+//     console.log(weatherData);
+// })
+
 //Display call in main card area.
 // var city = weatherData.name;
 // var windSpeed = weatherData.wind.windSpeed;
 // var humidity = weatherData.main.humidity;
 // var temp = weather.data.main.temp
 
-//Future forecast noted at the bottom in 5 cards. i per day
