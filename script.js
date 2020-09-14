3663fc9d96b78994fcdea88a40becd9aa3c1ab85
Alberte Laventure <alberte.laventure@gmail.com>
2020-09-14 18:30:13 -0400

    updated index, script, and style files

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-$(document).ready(function(){
+$("#add-city").on("click", function (event) {
+    event.preventDefault()
+    var city = $(this).siblings("#weather-input").val();
+    console.log(city);
+    var apiKey = "3c0180d657759505946b8abbfc2f67b7"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey+ "&units=imperial"
 
-    $("#add-city").on("click", function () {
-        console.log("Clicked");
-    var weatherInput =$("#weather-input").val(); 
-    var queryURL = "",
-    if (weatherInput !== ""){
-        $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/weather?id=" +weatherInput+ "990c20e7f9d2676532491d7e451773d2" + "&appid=" + "&units=imperial",
-                method: "GET",
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+
+            var city = response.name
+            var windSpeed = response.wind.speed
+            var humidity = response.main.humidity
+            var temperature = response.main.temp
+            
+
+            console.log(city);
+            console.log(windSpeed);
+            console.log(humidity);
+            console.log(temperature);
+
+            $(".city").html("<h3>City: " + city + "</h3>")
+            $(".temp").html("Temperature: " + temperature + "&deg")
+            $(".windSpeed").html("Wind Speed: " + windSpeed)
+            $(".humidity").html("Humidity: " + humidity)
+            
+
+            
+uvIndex(response.coord.lon, response.coord.lat)
         })
-        console.log(data)
-
-    } else {
-        $("#error").html("Please choose a city");
-    }
-    })
-    .then(function(){
-        console.log ($this);
-    })
 })
 
 
-    //Ajax call in seacrh box/onclick
+// function forecast(){
 
-// $.ajax({
-// url: "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=3c0180d657759505946b8abbfc2f67b7",
-// method:"GET"
-// })
-// .then(function(weatherData) {
-//     console.log(weatherData);
-// })
+// }
 
-//Display call in main card area.
-// var city = weatherData.name;
-// var windSpeed = weatherData.wind.windSpeed;
-// var humidity = weatherData.main.humidity;
-// var temp = weather.data.main.temp
 
+function uvIndex(long, lat) {
+    var apiKey = "3c0180d657759505946b8abbfc2f67b7"
+    var queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?&lat=" + lat + "&lon=" + long + "&appid=" + apiKey
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+var uv=response[0].value
+
+            $(".uvIndex").html("UV Index: " + uv )
+        })
+}
